2b16755369f0322a93176fa122a87152687f4dfc
Sve <kassidylewis7@gmail.com>
2025-09-09 12:40:17 -0700

    Revision 9/9

--- js/default.js
+++ js/default.js
@@ -2,12 +2,12 @@
 class CustomHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <nav id="menu">
-                <ul class="flex">
-                    <li style="margin-right: auto;"><a href="index.html" class="home-link">Home</a></li>
-                    <li><a href="design.html" class="menu-link link-anim">Design</a></li>
-                    <li><a href="illustration.html" class="menu-link link-anim">Illustration</a></li>
-                    <li><a href="about.html" class="menu-link link-anim">About</a></li>
+            <nav id="menu" class="bg-dark">
+                <ul class="flex j-between">
+                    <li style="margin-right: auto;"><a href="index.html" class="l-nav">Home</a></li>
+                    <li><a href="design.html" class="l-menu link-anim">Design</a></li>
+                    <li><a href="illustration.html" class="l-menu link-anim">Illustration</a></li>
+                    <li><a href="about.html" class="l-menu link-anim">About</a></li>
                 </ul>
             </nav>
             `
@@ -18,20 +18,23 @@ class CustomHeaderM extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="hamburger flex">
-                <i class="fa-solid fa-bars l-grey" onclick="openNav()" role="button"></i>
+                <button class="l-grey" onClick="openNav()"><i class="fa-solid fa-bars"></i></button>
             </div>
-            <nav id="mobilemenu">
-                <div class="closebtn">
-                    <i class="fa-solid fa-xmark l-mobile-nav" onclick="closeNav()" role="button"></i>
+            <nav id="mobilemenu" class="bg-dark">
+                <div class="mobile-content flex flex-column">
+                    <div class="closebtn">
+                        <button class="l-nav" onClick="closeNav()"><i class="fa-solid fa-xmark"></i></button>
+                    </div>
+                    <ul>
+                        <li class="mt-0"><i class="fa-solid fa-caret-right"></i><a href="index.html" class="l-nav">Home</a></li>
+                        <li><i class="fa-solid fa-caret-right"></i><a href="design.html" class="l-nav">Design</a></li>
+                        <li><i class="fa-solid fa-caret-right"></i><a href="illustration.html" class="l-nav">Illustration</a></li>
+                        <li><i class="fa-solid fa-caret-right"></i><a href="about.html" class="l-nav">About</a></li>
+                    </ul>
+                    <div class="socmed">
+                        <a class="l-nav" href="https://www.linkedin.com/in/kassidy-lewis/"><i class="fa-brands fa-linkedin"></i></a>
+                    </div>
                 </div>
-                <ul>
-                    <li><i class="fa-solid fa-caret-right"></i> <a href="index.html" class="l-mobile-nav">Home</a></li>
-                    <li><i class="fa-solid fa-caret-right"></i> <a href="design.html" class="l-mobile-nav">Design</a></li>
-                    <li><i class="fa-solid fa-caret-right"></i> <a href="illustration.html" class="l-mobile-nav">Illustration</a></li>
-                    <li><i class="fa-solid fa-caret-right"></i> <a href="about.html" class="l-mobile-nav">About</a></li>
-                </ul>
-                <div class="socmed">
-                    <a href="https://www.linkedin.com/in/kassidy-lewis/"><i class="fa-brands fa-linkedin l-mobile-nav"></i></a>
             </nav>
             `
     }
@@ -40,10 +43,10 @@ class CustomHeaderM extends HTMLElement {
 class CustomFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <footer>
+            <footer class="bg-dark">
                 <div class="flex j-between">
                     <div>&copy; 2025 Kassidy Lewis / <a href="https://www.linkedin.com/in/kassidy-lewis/" class="link-anim">LinkedIn</a></div>
-                    <div><a href="#top" onClick="scrollToTop()" class="l-mobile-nav" role="button">Top</a> <i class="fa-solid fa-caret-up"></i></div>
+                    <div><button href="#top" onClick="scrollToTop()" class="l-nav" style="border-bottom: 1px solid">Top</button> <i class="fa-solid fa-caret-up"></i></div>
                 </div>
             </footer>
             `
@@ -53,7 +56,7 @@ class CustomFooter extends HTMLElement {
 class GetinTouch extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <section id="getintouch">
+            <section id="getintouch" class="bg-white">
                 <div class="container">
                     <h1>Get in Touch!</h1>
                     <p class="getintouch-text">Think I'd be a great fit for your next project? Message me <address><a id="emailLinkID" href=" ">via email</a></address> or connect with me via <a href="https://www.linkedin.com/in/kassidy-lewis/">LinkedIn</a>!</p>
