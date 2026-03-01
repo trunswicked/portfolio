878442f04d76fd9103040983355d15037de0d1fc
Sve <kassidylewis7@gmail.com>
2026-03-01 12:04:33 -0700

    style: portrait image height adjusted
    * chore: Resume file updated.
    * style: Index hero section re-aligned.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <>
             <h2 id="contact" className="font-display text-4xl">Get in Touch!</h2>
-            <p className="py-4">Think I'd be a great fit for your next project? <a href="mailto:trunswickedart@gmail.com" class="font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">Contact me by email</a> or <a href="https://www.linkedin.com/in/kassidy-lewis/" class="font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">connect with me on LinkedIn</a>!</p>
+            <p className="py-4">Think I'd be a great fit for your next project? <a href="mailto:trunswickedart@gmail.com" className="font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">Contact me by email</a> or <a href="https://www.linkedin.com/in/kassidy-lewis/" className="font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">connect with me on LinkedIn</a>!</p>
             <div className="flex justify-between font-semibold">
                 <span>&#0169; 2026 Kassidy Lewis</span>
                 <a href="#top" className="hover:text-teal hover:underline decoration-2 underline-offset-3 transition duration-300">
