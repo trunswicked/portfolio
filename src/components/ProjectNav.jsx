c62c8f02d7a7b9ab028947c1417d6f6b330e72cb
Sve <kassidylewis7@gmail.com>
2026-03-30 16:45:59 -0700

    feat: Fairchild Website project page added
    Added project page for Fairchild Website.
    
    * chore: dependencies updated.
    * feat: image lightbox implemented. enlargeable images given hover anim.
    * style: images on design project pages given a rounded border.
    * style: ProjectItems on About page converted into simple image elements.
    * style: 3rd-level headings given accompanying icons.

--- src/components/ProjectNav.jsx
+++ src/components/ProjectNav.jsx
@@ -3,7 +3,7 @@ export default function ProjectNav(props) {
         <div className="flex justify-between">
             <div>
                 <a href={props.link_left}>
-                    <img src={props.src_left} alt={props.alt_left} className="h-20 md:h-30 object-cover rounded pb-2"></img>
+                    <img src={props.src_left} alt={props.alt_left} className="h-20 md:h-30 object-cover rounded pb-2 hover:opacity-75 transition-opacity"></img>
                 </a>
                 <a href={props.link_left} className="font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
                     <i className="fa-solid fa-angles-left mr-1 text-xs"></i>{props.name_left}
@@ -11,7 +11,7 @@ export default function ProjectNav(props) {
             </div>
             <div className="text-right">
                 <a href={props.link_right}>
-                    <img src={props.src_right} alt={props.alt_right} className="ml-auto h-20 md:h-30 object-cover rounded pb-2"></img>
+                    <img src={props.src_right} alt={props.alt_right} className="ml-auto h-20 md:h-30 object-cover rounded pb-2 hover:opacity-75 transition-opacity"></img>
                 </a>
                 <a href={props.link_right} className="font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
                     {props.name_right}<i className="fa-solid fa-angles-right ml-1 text-xs"></i>
