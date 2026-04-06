d4af2c38efb39c539e34a8019c54d3fce6d7bb0b
Sve <kassidylewis7@gmail.com>
2026-04-06 13:02:13 -0700

    style: fixed ProjectItem border-radius

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,16 +1,16 @@
 export default function ProjectItem(props) {
     return (
-        <div className="relative overflow-hidden group">
-            <div className="aspect-video rounded bg-grey/25">
+        <div className="relative overflow-hidden group rounded">
+            <div className="aspect-video bg-grey/25">
                 <a href={props.url}>
                     <img 
                         src={props.src}
                         alt={props.alt}
-                        className="aspect-video object-cover rounded-t"
+                        className="aspect-video object-cover"
                     />
                 </a>
             </div>
-            <div className="p-6 absolute top-full right-0 left-0 h-full rounded-t text-white bg-black/90 group-hover:top-0 transition-all ease-in-out duration-300">
+            <div className="p-6 absolute top-full right-0 left-0 h-full text-white bg-black/90 group-hover:top-0 transition-all ease-in-out duration-300">
                 <div className="absolute bottom-0 mb-6">
                     <a href={props.url}>
                         <h4 className="font-display text-2xl text-teal hover:underline underline-offset-4">
--- src/components/SubNavigation.jsx
+++ src/components/SubNavigation.jsx
@@ -1,20 +1,20 @@
 export default function SubNavigation(props) {
     return (
-        <nav class="my-12 p-6 text-center bg-black text-white" aria-labelledby="sub-navigation">
+        <nav className="my-12 p-6 text-center bg-black text-white" aria-labelledby="sub-navigation">
             <em>Jump to:</em>
-            <ul class="pt-2 flex justify-center divide-x-3 divide-grey font-semibold transition-all">
+            <ul className="pt-2 flex justify-center divide-x-3 divide-grey font-semibold transition-all">
                 <li>
-                    <a href={props.link_1} class="pr-4 font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
+                    <a href={props.link_1} className="pr-4 font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
                         {props.label_1}
                     </a>
                 </li>
                 <li>
-                    <a href={props.link_2} class="px-4 font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
+                    <a href={props.link_2} className="px-4 font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
                         {props.label_2}
                     </a>
                 </li>
                 <li>
-                    <a href={props.link_3} class="pl-4 font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
+                    <a href={props.link_3} className="pl-4 font-semibold hover:text-teal hover:underline decoration-2 underline-offset-3 transition-all duration-300">
                         {props.label_3}
                     </a>
                 </li>
